39136f3c4d892516fdd271eaf59f765adc38027d
4ndr01d3 <gustavoadolfo.salazar@gmail.com>
2015-06-08 14:41:07 +0200

    first basic html

--- bbc/tests.py
+++ bbc/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from bbc.views import home_page
@@ -7,3 +8,10 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        response = home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>BBCatalog</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))  #5
