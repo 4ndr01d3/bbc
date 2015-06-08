e794704252846a10ea2596c032af87aec92eb97b
4ndr01d3 <gustavoadolfo.salazar@gmail.com>
2015-06-08 16:06:00 +0200

    using templates

--- bbc/tests.py
+++ bbc/tests.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from bbc.views import home_page
@@ -10,8 +11,7 @@ class SmokeTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  #1
-        response = home_page(request)  #2
-        self.assertTrue(response.content.startswith(b'<html>'))  #3
-        self.assertIn(b'<title>BBCatalog</title>', response.content)  #4
-        self.assertTrue(response.content.endswith(b'</html>'))  #5
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
