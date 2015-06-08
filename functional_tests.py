4abf3ba5a51b36e222c807035e9214c464501b83
4ndr01d3 <gustavoadolfo.salazar@gmail.com>
2015-06-08 14:15:59 +0200

    functional tests as classes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+#As an External researcher, I want search studies so that I can find studies that include samples of my interest
+class NewVisitorTest(unittest.TestCase):  #1
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_search_studies_and_select_the_result(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('BBCatalog', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # Follows the link to search studies
+
+        # Inputs a complicated text in the general text search
+
+        # There are not results for it
+
+        # chooses some parameters from the form
+
+        # there are results
+
+        # Chooses one of them
+
+        # Gets the needed information.
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
